173c0f203913aa68560b47c318042a0d1682cf66
Notavone <notavone@gmail.com>
2023-06-13 08:31:32 +0200

    vfnvjl,efvlb

--- feur.js
+++ feur.js
@@ -1,11 +1,15 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js");
+    navigator.serviceWorker.register("sw.js")
+        .then((sw) => sw
+            .addEventListener("updatefound", () => console.debug("Mise à jour du service worker trouvée")));
 }
 
 const inputQuoi = document.getElementById('quoi');
 const labelQuoi = document.getElementById('labelQuoi');
 const leBody = document.getElementById('leBody');
 const avertissementRelou = document.getElementById('avertissementRelou');
+const laRegexQuoi = /^quoi *?\?/i;
+const nbFeurDebug = undefined; // mettre un chiffre pour forcer ce nombre de feur à apparaitre ou laisser undefined pour un nombre aléatoire.
 
 document.addEventListener("DOMContentLoaded", () => {
     inputQuoi.focus();
@@ -13,13 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
         inputQuoi.focus();
     });
     inputQuoi.addEventListener('input', (e) => {
-        if ("QUOI?" === e.target.value.trim().toUpperCase()
-            || "QUOI ?" === e.target.value.trim().toUpperCase()) {
+        if (!laRegexQuoi.test(e.target.value)) {
+            e.preventDefault();
+        } else {
             showTime();
         }
     });
 
-    const nbFeur = undefined ?? Math.floor(leBody.getBoundingClientRect().width * leBody.getBoundingClientRect().height / 5000);
+    const nbFeur = nbFeurDebug ?? Math.floor(leBody.getBoundingClientRect().width * leBody.getBoundingClientRect().height / 5000);
 
     for (let i = 0; i < nbFeur; i++) {
         const ceFeur = document.createElement('h1');
@@ -36,35 +41,75 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+/**
+ *
+ * @returns {string}
+ */
 function ecritureFeurAleatoire() {
     const tableauFeur = ['feur', 'feuR', 'feUr', 'feUR', 'fEur', 'fEuR', 'fEUr', 'fEUR', 'Feur', 'FeuR', 'FeUr', 'FeUR', 'FEur', 'FEuR', 'FEUr', 'FEUR', 'feur!', 'feuR!', 'feUr!', 'feUR!', 'fEur!', 'fEuR!', 'fEUr!', 'fEUR!', 'Feur!', 'FeuR!', 'FeUr!', 'FeUR!', 'FEur!', 'FEuR!', 'FEUr!', 'FEUR!',];
     return tableauFeur[Math.floor(Math.random() * tableauFeur.length)];
 }
 
+/**
+ *
+ * @returns {string}
+ */
 function couleurCSSAleatoire() {
     return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
 }
 
+/**
+ *
+ * @param {number} tailleTexte
+ * @returns {number}
+ */
 function yAleatoire(tailleTexte) {
     const hauteurPage = leBody.getBoundingClientRect().height - tailleTexte;
     return Math.floor(Math.random() * hauteurPage);
 }
 
+/**
+ *
+ * @returns {boolean}
+ */
+function pileOuFace() {
+    return !!Math.round(Math.random());
+}
+
+/**
+ *
+ * @param {number} tailleTexte
+ * @returns {number}
+ */
 function xAleatoire(tailleTexte) {
     const largeurPage = leBody.getBoundingClientRect().width - tailleTexte;
     return Math.floor(Math.random() * largeurPage);
 }
 
+/**
+ *
+ * @returns {number}
+ */
 function rotationAleatoire() {
     return Math.floor(Math.random() * 90) - 45;
 }
 
+/**
+ *
+ * @param {HTMLHeadingElement} ceFeur
+ */
 function majFeur(ceFeur) {
     ceFeur.innerHTML = ecritureFeurAleatoire();
     ceFeur.style.transform = `rotate(${rotationAleatoire()}deg)`;
     ceFeur.style.color = couleurCSSAleatoire();
     ceFeur.style.left = xAleatoire(ceFeur.getBoundingClientRect().width + 20) + "px";
     ceFeur.style.top = yAleatoire(ceFeur.getBoundingClientRect().height + 20) + "px";
+
+    if (pileOuFace()) {
+        ceFeur.style.fontWeight = "bold";
+    } else {
+        ceFeur.style.fontWeight = "normal";
+    }
 }
 
 function majLeBody() {
@@ -91,6 +136,11 @@ function showTime() {
 
 }
 
+/**
+ *
+ * @param {() => void} callback
+ * @param {number} interval
+ */
 function intervalAvecAnimationFrame(callback, interval) {
     let lastTime = 0;
 
